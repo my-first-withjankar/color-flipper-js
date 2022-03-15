cb899284310986c64f19afa160c75165da85dcfe
jankar-with-tarek <avirus170@gmail.com>
2022-03-15 13:10:37 +0600

    added main and simple tab

--- setup/app.js
+++ setup/app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function () {
+    // console.log(document.body);
+    const random = getRandomNumber()
+    console.log(random);
+    document.body.style.backgroundColor = colors[random]
+    color.textContent = colors[random]
+})
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length)
+}
